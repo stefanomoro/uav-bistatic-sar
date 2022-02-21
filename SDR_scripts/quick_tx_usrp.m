19c9f44589bdd14ce34377dc3afd8e7786a11b63
Stefano <morostefano96@gmail.com>
2022-02-21 15:41:39 +0100

    feat: updated usrp_tx with power 2 tx_waveform

--- SDR_scripts/quick_tx_usrp.m
+++ SDR_scripts/quick_tx_usrp.m
@@ -1,53 +1,56 @@
+%% COSTANTS
 SampleRate = 30e6;
-fc=2.385e9;
-snGTX='sn:1044735411960009090038004f371e8d27';
+fc=1.65e9;
 disp("Begin setup!")
-% Old Chirp Generation
-% t1=3e5;
-% t=1:1:t1;
-% f0=1;
-% f1=4e6;
-% s1= chirp(t,f0,t1,f1,'complex').';
-% s=s1;
-% 
-% % Chirp Generation (Prof)
-% fs = SampleRate; % f campionamento
-% dt = 1/fs;
-% B = fs/1.5; % banda totale
-% T = 1e-3; % durata del chirp
-% K = B/T;
-% t = (-T/2:dt:T/2);
-% Tc = T * 0.8;
-% s = exp(1i*pi*K*t.^2);% .* rectpuls(t/Tc);
-% s_pad=zeros(length(s)+ceil(length(s)*0.1),1);
-% s_pad(1:length(s))=s;
-
-% % to plot spectrum
-% S = fftshift(fft(s));
-% plot(abs(S))
-
-% Normalize (check max in USRP?)
-txWave = load("../tx_waveform/tx_waveform_B27M_S30M").s_pad;
-% txNorm=transpose(0.5*s_pad/max(abs(s_pad)));
-% S = fftshift(fft(s));
-% plot(abs(S))
 
 
+% Chirp Generation (Prof)
+fs = SampleRate; % f campionamento
+dt = 1/fs;
+B = fs * 0.9; % banda totale
+samples_per_chirp = 2^15;
+T_tot = samples_per_chirp * dt; % durata del chirp
+T = T_tot * .9;
+K = B/T;
+t = (-T/2:dt:T/2);
+
+s = exp(1i*pi*K*t.^2);
+s_pad=zeros(samples_per_chirp,1);
+s_pad(1:length(s))=s;
+
+% save(strcat("tx_waveform_B",num2str(B/1e6),"M_S",num2str(fs/1e6),"M"),"s_pad")
+% to plot spectrum
+S = fftshift(fft(s));
+plot(abs(S))
+
+%% Normalize (max in USRP is 1)
+
+txNorm=0.99*s_pad/max(abs(s_pad));
+
+%%%%%%%%%%
+TxPow = 3;              % power in dBm
+%%%%%%%%%%
+MaxPow = 10;            % max power USRP ca transmit is 10dbm
+MaxGain = 89.75;        % gain associated to max tx power
+TxGain = MaxGain - (MaxPow - TxPow);
+%% RADIO SETUP
 
 tx = comm.SDRuTransmitter('Platform','B210', ...
-              'SerialNum','3218D18', ...
-              'Gain',60,...  %min 0 max 89.75 (step 0.25)
-              'CenterFrequency',fc, ...
-              'MasterClockRate',fs, ...
-              ...'SamplesPerFrame',frame_len,...
-              ...%'EnableBurstMode',1,...
-              ...%'NumFramesInBurst',burst_n,...
-              'TransportDataType','int8',...
-              'InterpolationFactor',1 ...
-              );
+          'SerialNum','3218D18', ...
+          'Gain',TxGain,...  %min 0 max 89.75 (step 0.25)
+          'CenterFrequency',fc, ...
+          'MasterClockRate',fs, ...
+          ...'SamplesPerFrame',frame_len,...
+          ...'EnableBurstMode',1,...
+          ...'NumFramesInBurst',burst_n,...
+          'PPSSource','GPSDO',...
+          'ClockSource','GPSDO',...
+          'TransportDataType','int8',...
+          'InterpolationFactor',1 ...
+);
+          
 
-
-
+%% TRANSMISSION
 disp("Begin Transmission!")     
 while 1
     res = tx(txNorm);
@@ -55,4 +58,3 @@ while 1
         disp("underrun detected")
     end
 end
-%transmitRepeat(tx,txNorm); %Non va sulle USRP
