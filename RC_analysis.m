72c211b81930179e0a36ba43193df5be44f402ee
Stefano <morostefano96@gmail.com>
2022-03-03 10:23:11 +0100

    feat: updated phase correction. Still not perfect

--- RC_analysis.m
+++ RC_analysis.m
@@ -7,7 +7,7 @@ chirp_sr = 30e6;                                % SDR sample rate
 norm_B = chirp_bw / chirp_sr;
 % experiment_name = strcat('MULTISTATIC_GPS1_B',...
 %     num2str(chirp_bw/1e6),'M_S',num2str(chirp_sr/1e6),'M');
-experiment_name = "data5_cut1";
+experiment_name = "data6_cut1";
 % folder_name = 'mat_files/radar_window_test/';
 folder_name = 'mat_files/giuriati_test_22_02_25/RC/cut/';
 
@@ -31,17 +31,10 @@ RC_file = strcat(folder_name,experiment_name);
 
 RC = load_bin(RC_file);
 % RC = RC(:,1:98273);
-%% ESTIMATE err1
-% estimate only checking starting and ending idx of the peak
-
-[~,st_pos] = max(abs(RC(:,1)));
-[~,end_pos] = max(abs(RC(:,end)));
-diff = abs(end_pos - st_pos);
-err1= diff / (samples_per_chirp * size(RC,2));
-clear diff st_pos end_pos
 %% PEAK time-shift tracking
 [~,peak_idx] = max(abs(RC(:,1)));
-RC_peak = RC(peak_idx-10:peak_idx+10,:);
+peak_margin = 5;
+RC_peak = RC(peak_idx-peak_margin:peak_idx+peak_margin,:);
 OSF = 8;                                        %over sampling factor
 %% Peak interpolation
 x = 0:dt:dt*(size(RC_peak,1) -1);
@@ -54,6 +47,13 @@ RC_peak_OS = W*RC_peak;
 [~,peak_idxs] = max(RC_peak_OS);
 peak_idxs = movmean(peak_idxs,2000); 
 peak_shifts = peak_idxs -peak_idxs(1);
+
+%% ESTIMATE err1
+% estimate only checking starting and ending idx of the peak
+diff = abs(peak_shifts(end));
+err1= diff / (samples_per_chirp * OSF *  size(RC,2));
+clear diff
+
 %% TIME SHIFT CORRECTION
 t = 0:size(RC,1) -1;
 tf = 0:1/OSF:(size(RC,1) -1);
@@ -73,19 +73,16 @@ f = (-Nf/2:Nf/2 - 1)/Nf;
 H = exp(1i*2*pi*f(:)*peak_shifts);
 H(1,:) = 0;                         % 
 
-RC_fix = ifft(ifftshift(X.* H,1),Nf,1);
-RC_fix = RC_fix(1:size(RC_OS,1),:);
+RC_Dt_fixed = ifft(ifftshift(X.* H,1),Nf,1);
+RC_Dt_fixed = RC_Dt_fixed(1:size(RC_OS,1),:);
 
-figure,
-subplot(2,1,1)
-imagesc(abs(RC_OS))
-subplot(2,1,2)
-imagesc(abs(RC_fix))
-%% 
-
-out_file = strcat('mat_files/radar_window_test/',...
-    experiment_name,'/RC/RC_time_fix');
-save_bin(out_file,RC_fix)
+%% Df correction
+time_shifts = peak_shifts * dt/OSF;
+polinom = polyfit(0:length(time_shifts)-1,time_shifts,7);
+fitted_shifts = polyval(polinom,0:length(time_shifts)-1);
+%%
+phase_fixer = repmat(exp(-2i* pi*f0*fitted_shifts),size(RC_Dt_fixed,1),1);
+RC_Df_fixed = RC_Dt_fixed .* phase_fixer;
 
 %% PLOTTING
 R_ax = -R_margin:dR/OSF:R_margin;
@@ -96,65 +93,25 @@ figure,subplot(2,1,1)
 imagesc(tau_ax,R_ax,abs(circshift(RC_OS,samp_shift,1)));
 title('RC original' ),xlabel("Slow time [s]"),ylabel("Range [m]")
 subplot(2,1,2)
-imagesc(tau_ax,R_ax,abs(circshift(RC_fix,samp_shift,1)));
+imagesc(tau_ax,R_ax,abs(circshift(RC_Dt_fixed,samp_shift,1)));
 title('RC time fixed' ),xlabel("Slow time [s]"),ylabel("Range [m]")
+%% phase 
+figure,subplot(2,1,1)
+imagesc(tau_ax,R_ax,angle(circshift(RC_Dt_fixed,samp_shift,1)));
+title('RC Dt fixed' ),xlabel("Slow time [s]"),ylabel("Range [m]")
+subplot(2,1,2)
+imagesc(tau_ax,R_ax,angle(circshift(RC_Df_fixed,samp_shift,1)));
+title('RC freq fixed' ),xlabel("Slow time [s]"),ylabel("Range [m]")
+%% Peak Phase
+figure,subplot(2,1,1),
+plot(tau_ax,angle(RC_OS(peak_idx(1),:)))
+title('Peak phase original' ),xlabel("Slow time [s]"),ylabel("Phase [rad]")
 
-%% VARIABLE Df computation
-win_size = 1024;
-RC_centered = circshift(RC_fix,samp_shift,1);
+subplot(2,1,2),
+plot(tau_ax,angle(RC_Df_fixed(peak_idx(1),:)))
+title('Peak phase fixed' ),xlabel("Slow time [s]"),ylabel("Phase [rad]")
 
-[ ~, idx ] = max(RC_centered(:,1));
-peak = RC_centered(idx,:);
-% compare magnitude of peak with closer values in t to see its stability
-figure,plot(abs(peak)),hold on, plot(abs(RC_centered(idx + 1,:))),plot(abs(RC_centered(idx - 1,:)))
-legend("Peak at start","Peak + 1 samples","Peak - 1 samples")
-%%
-% number of shifts of the window over the peak
-N_cycle = floor(length(peak)/win_size) * 2 - 1;
-Df = zeros(N_cycle,1);
-fft_max = Df;   fft_angle = Df;
-N_fft = 8*win_size;
-freq_ax = linspace(-.5,.5,N_fft) * PRF;
-win_idx = 1 : win_size;
-for i = 1:N_cycle
-    aa = peak(win_idx);
-    ff = fftshift(fft(aa,N_fft));
-    [fft_max(i),ii] = max(abs(ff));
-    fft_angle(i) = angle(ff(ii));
-    Df(i) = freq_ax(ii);
-    win_idx = win_idx + win_size/2;
-% 	figure,plot(freq_ax,abs(ff));
-end
-
-%%
-figure,plot(Df);title("Df values"),xlabel("Sliding window iteration"),ylabel("Frequency [Hz]"),grid on
-figure,plot(fft_max);title("FFT peak amplitude");xlabel("Sliding Window iteration"), ylabel("Amplitude"),grid on
-figure,plot(fft_angle);title("FFT peak angle ");xlabel("Sliding Window iteration"),ylabel("Phase [rad]"),grid on
-
-%% FIT Df curve
-Df_x = linspace(1,length(peak),length(Df));
-Df_x1 = 1:length(peak);
-ord = 20;
-Df_poly=   polyfit(Df_x,Df,ord);
-Df_y1 = polyval(Df_poly,Df_x1);
-figure,plot(Df_x,Df,Df_x1,Df_y1);
-title(strcat("Order ",num2str(ord)," poly fitting "))
-
-%% Df fix
-% phasor = exp(-1i* 2*pi * Df_y1 .* tau_ax);
-phasor = exp(-1i*angle(peak));
-% peak_fixed = peak.* phasor;
-% figure,plot(unwrap(angle(peak))),hold on,plot(unwrap(angle(peak_fixed)));
-
-phasor_mat = repmat(phasor,size(RC_centered,1),1);
-RC_Df_fixed = RC_centered .* phasor_mat;
-%% Plot
-figure,
-subplot(2,1,1);imagesc(tau_ax,R_ax,angle(RC_centered));title("Original")
-subplot(2,1,2);imagesc(tau_ax,R_ax,angle(RC_Df_fixed));title("Df fixed")
-figure,
-subplot(2,1,1);imagesc(tau_ax,R_ax,abs(RC_centered));title("Original")
-subplot(2,1,2);imagesc(tau_ax,R_ax,abs(RC_Df_fixed));title("Df fixed")
+return
 %% Coherent SUM
 coh_sum = sum(RC_Df_fixed,2);
 
