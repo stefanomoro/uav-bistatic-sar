f8812e70b95dd416a74050798b490514cee6d8c0
Stefano <morostefano96@gmail.com>
2022-09-14 09:24:41 +0200

    update: updated main uav_processing

--- uav processing/main.m
+++ uav processing/main.m
@@ -1,13 +1,16 @@
 clear variables
 close all
 clc
-%% =============================================================================  LOAD RC MATRIX CUT IN TARGET ZONE
+
+addpath(genpath('..\lib' ));       % add path of lib
+addpath("./utils")
+
 %% CONSTANTS
-param = load("./test_parameters/20220502/test1_pass2_param").param;
+param = load("./test_parameters/20220826/test6").param;
+
 [const] = initializeConstants(param);
 
 %% PRE-PROCESSING
-addpath(genpath('..\lib' ));       % add path of lib
 [radar,drone,targets] = loadData(const);
 % Cut processed RC
 [radar] = cutProcessedRC(radar,param);
@@ -18,7 +21,6 @@ plotRC(radar,[],1)
 [scenario] = computeAllDistances(const,radar,RX,TX,targets);
 %Rotate path
 [RX,TX,targets,drone] = rotateFlightPath(RX,TX,targets,drone);
-plotDroneTrack(drone,RX,TX,targets);
 
 %% RC Synch, correct and prepare for Focusing
 [radar]= interpolateRC(const,radar);
@@ -26,23 +28,29 @@ plotDroneTrack(drone,RX,TX,targets);
 [radar] = timeShiftCorrection(radar);
 [radar] = getCrossTalk(radar);
 [radar] = freqShiftCorrection(const,radar);
-% [radar] = filterGaussRC(const,radar,.75*const.dt);
+plotDroneTrack(drone,RX,TX,targets);
+[radar] = filterGaussRC(const,radar,.75*const.dt);
+
+plotRC(radar,scenario,1);
 
 plotRC(radar,scenario,2);
 plotRC(radar,scenario,3);
 
 
 %% FOCUSING
-[scenario] = defineFocusingGrid(const,scenario);
+[scenario] = defineFocusingGrid(const,scenario,RX);
 [focus] = focusingTDBP_GPU(const,radar,scenario,RX,TX);
-figure,plotFocusedWithTargets(scenario,RX,TX,targets,focus.Focused_vec{1},...
-        "0 doppler, not equalized");
-
+figure,plotFocusedWithTargets(scenario,RX,TX,targets,20*log10(focus.Focused_vec(:,:,8)),...
+        "0° squint, not equalized");
+caxis([100 160])
 % EQUALIZE distance
 [focus] = equalizeDistanceRC(scenario,TX,focus);
 
+%% Convert cellarray to matrix
+if(iscell(focus.Focused_vec))
+    focus = convertCellToMatrix(focus);
+end
 %% SUM all the focused angles
-
 focus_all_sum = sumAllFocusedAngle(focus.Focus_eq,1);
 
 figure,plotFocusedWithTargets(scenario,RX,TX,targets,focus_all_sum,...
@@ -57,20 +65,29 @@ figure,plotFocusedWithTargets(scenario,RX,TX,targets,...
 
 caxis([180 230]) 
 
-%% save subplot squint
+%% Show 
 figure
-for idx = 3:8
-subplot(2,3,idx-2)
-% F = filterHammingFocus(focus.Focus_eq{idx},3);
-F = focus.Focus_eq{idx};
+for idx = 1:8
+subplot(2,4,idx)
+F = filterHammingFocus(focus.Focus_eq(:,:,idx),3);
+% F = focus.Focused_vec(:,:,idx);
 plotFocusedWithTargets(scenario,RX,TX,targets,20*log10(F),...
         strcat("Squint ",num2str(focus.angle_vec(idx)) ,"°, dB"));  
-    caxis([160 220])
+    caxis([100 200])
+end
+figure
+for idx = 8:15
+subplot(2,4,idx-7)
+F = filterHammingFocus(focus.Focus_eq(:,:,idx),3);
+% F = focus.Focused_vec(:,:,idx);
+plotFocusedWithTargets(scenario,RX,TX,targets,20*log10(F),...
+        strcat("Squint ",num2str(focus.angle_vec(idx)) ,"°, dB"));  
+    caxis([100 200])
 end
 
 %%
-makeGIF(const,scenario,RX,TX,targets,focus,2);
-saveAllSquintImages(const,scenario,RX,TX,targets,focus,2);
+makeGIF(const,scenario,RX,TX,targets,focus,2,[100 200]);
+% saveAllSquintImages(const,scenario,RX,TX,targets,focus,2);
 %% SAVE vec
 save( getResultsFileName(focus),"param","const","scenario","focus")
 
